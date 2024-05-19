a0943850d097094e42098b74804b84d294640278
Abhirup Kumar Bhowmick <abhirupkumar2003@gmail.com>
2024-05-19 17:04:15 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 import MaxWidthWrapper from './components/MaxWidthWrapper';
+import Navbar from './components/Navbar';
 
 export default function App() {
   return (
     <>
-      <MaxWidthWrapper className='mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center'>
+      <Navbar />
+      <MaxWidthWrapper className='mb-12 mt-28 flex flex-col items-center justify-center text-center'>
         <div className='mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/90'>
           <p className='text-sm font-semibold text-gray-700'>
-            DocWhisperer is now open in beta!
+            AirRoute is now public!
           </p>
         </div>
         <h1 className='max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl'>
-          Chat with your{' '}
-          <span className='text-blue-600'>docs</span>{' '}
+          Check out your flight{' '}
+          <span className='bg-gradient-to-r from-[#FD9248] via-[#FA1768] to-[#F001FF] bg-clip-text text-transparent'>route</span>{' '}
           in seconds.
         </h1>
         <p className='mt-5 max-w-prose text-zinc-300 sm:text-lg'>
-          DocWhisperer allows you to speak with any PDF document. Simply upload your files and start asking questions right away.
+          AirRoute allows you to find out the best possible route amidst all the traffic. Simply provide your starting point and destination to know your route.
         </p>
 
         <a
-          className=""
+          className="inline-flex items-center justify-center whitespace-nowrap rounded-full text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-11 rounded-full px-8 mt-5"
           to='dashboard'>
           Get started
         </a>
