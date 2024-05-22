27db771aec29e74f74f5c6c71c4c074565928723
Abhirup Kumar Bhowmick <abhirupkumar2003@gmail.com>
2024-05-22 10:57:25 +0530

    added airport data

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,69 +1,75 @@
 import React, { useState } from 'react';
+import { ReactSearchAutocomplete } from 'react-search-autocomplete';
+import airports from '../data/airports.json';
 
 const InputForm = ({ onSubmit }) => {
-    const [startLat, setStartLat] = useState('');
-    const [startLng, setStartLng] = useState('');
-    const [endLat, setEndLat] = useState('');
-    const [endLng, setEndLng] = useState('');
+    const airportsData = airports["airports"];
+    console.log(airportsData)
+    const [start, setStart] = useState(null);
+    const [dest, setDest] = useState(null);
+
+    const handleOnSelectStartAirport = (item) => {
+        setStart(item);
+    }
+
+    const handleOnSelectEndAirport = (item) => {
+        setDest(item);
+    }
+
+    const formatResult = (item) => {
+        return (
+            <>
+                <span className="block text-left z-10">{item.name}</span>
+            </>
+        )
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        onSubmit(startLat, startLng, endLat, endLng);
-    };
+        if (!start || !dest) {
+            return;
+        }
+        onSubmit(start.Latitude, start.Longitude, dest.Latitude, dest.Longitude);
+    }
+
+    const handleClear1 = () => {
+        setStart(null);
+    }
+    const handleClear2 = () => {
+        setDest(null);
+    }
 
     return (
-        <form onSubmit={handleSubmit} className=" flex flex-wrap w-full backdrop-blur-[1px] items-center justify-center gap-6 max-w-lg shadow-md border-2 border-zinc-600 rounded px-8 pt-6 pb-8 mb-4">
-            <div className="mb-4">
-                <label className="block text-gray-300 text-sm font-semibold mb-2" htmlFor="startLat">
-                    Start Latitude
-                </label>
-                <input
-                    type="number"
-                    id="startLat"
-                    value={startLat}
-                    onChange={(e) => setStartLat(e.target.value)}
-                    className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline bg-background"
-                    required
-                />
-            </div>
-            <div className="mb-4">
-                <label className="block text-gray-300 text-sm font-semibold mb-2" htmlFor="startLng">
-                    Start Longitude
-                </label>
-                <input
-                    type="number"
-                    id="startLng"
-                    value={startLng}
-                    onChange={(e) => setStartLng(e.target.value)}
-                    className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline bg-background"
-                    required
-                />
-            </div>
-            <div className="mb-4">
-                <label className="block text-gray-300 text-sm font-semibold mb-2" htmlFor="endLat">
-                    Destination Latitude
-                </label>
-                <input
-                    type="number"
-                    id="endLat"
-                    value={endLat}
-                    onChange={(e) => setEndLat(e.target.value)}
-                    className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline bg-background"
-                    required
-                />
-            </div>
-            <div className="mb-4">
-                <label className="block text-gray-300 text-sm font-semibold mb-2" htmlFor="endLng">
-                    Destination Longitude
-                </label>
-                <input
-                    type="number"
-                    id="endLng"
-                    value={endLng}
-                    onChange={(e) => setEndLng(e.target.value)}
-                    className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline bg-background"
-                    required
-                />
+        <form onSubmit={handleSubmit} className="flex flex-col w-full backdrop-blur-[1px] items-center justify-center gap-6 max-w-lg shadow-md border-2 border-zinc-600 rounded px-8 pt-6 pb-8 mb-4">
+            <div className='flex flex-col w-full'>
+                <div className="mb-4 w-full">
+                    <label className="block text-gray-300 text-sm font-semibold mb-2" htmlFor="start">
+                        Starting Airport
+                    </label>
+                    <ReactSearchAutocomplete
+                        items={airportsData}
+                        fuseOptions={{ keys: ["name", "id"] }}
+                        onSelect={handleOnSelectStartAirport}
+                        onClear={handleClear1}
+                        autoFocus
+                        formatResult={formatResult}
+                        resultStringKeyName="name"
+                    />
+                </div>
+                <div className="mb-4">
+                    <label className="block text-gray-300 text-sm font-semibold mb-2" htmlFor="dest">
+                        Destination Airport
+                    </label>
+                    <ReactSearchAutocomplete
+                        items={airportsData}
+                        fuseOptions={{ keys: ["name"] }}
+                        onSelect={handleOnSelectEndAirport}
+                        onClear={handleClear2}
+                        autoFocus
+                        formatResult={formatResult}
+                        resultStringKeyName="name"
+                    />
+                </div>
             </div>
             <div className="flex items-center justify-between">
                 <button type="submit" className="inline-flex items-center justify-center whitespace-nowrap rounded-[10px] text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-11 px-8">
